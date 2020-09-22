de9ce6ba531e5cfa60a419f585eab0241650f190
Hidalgo Mac <hidalgoponce@icloud.com>
2020-09-22 12:56:46 -0500

    Update

--- resources/views/layouts/page/app.blade.php
+++ resources/views/layouts/page/app.blade.php
@@ -19,10 +19,10 @@
     <link rel="apple-touch-icon" sizes="144x144" href="{{asset('favicon/apple-icon-144x144.png')}}">
     <link rel="apple-touch-icon" sizes="152x152" href="{{asset('favicon/apple-icon-152x152.png')}}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-icon-180x180.png')}}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('favicon/android-icon-192x192.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('favicon/favicon-96x96.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+{{--    <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('favicon/android-icon-192x192.png')}}">--}}
+{{--    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">--}}
+{{--    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('favicon/favicon-96x96.png')}}">--}}
+{{--    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">--}}
     <link rel="icon" type="image/x-icon" href="{{asset('favicon/favicon.ico')}}">
 
     <link rel="manifest" href="{{asset('favicon/manifest.json')}}">
